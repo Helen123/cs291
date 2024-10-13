87d61663d0d16365bbc801f34e41c1d9d452fe2e
apple <apple@169-231-21-221.wireless.ucsb.edu>
2024-10-13 14:04:47 -0700

    4

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # Sort the sides to simplify the logic
+  sides = [a, b, c].sort
+
+  # Raise TriangleError for invalid triangles
+  if sides.any? { |side| side <= 0 } || sides[0] + sides[1] <= sides[2]
+    raise TriangleError
+  end
+
+  # Determine the type of triangle
+  if sides.uniq.length == 1
+    :equilateral
+  elsif sides.uniq.length == 2
+    :isosceles
+  else
+    :scalene
+  end
 end
 
-# Error class used in part 2.  No need to change this code.
+# Error class used in part 2. No need to change this code.
 class TriangleError < StandardError
 end
+
